a1123700a815c4b86f452f77b31514e91b809b33
Yusuf Fakhriddinov <135459719+yusuffakhriddinov@users.noreply.github.com>
2024-03-22 21:04:07 +0000

    Yusuf

--- include/hw3.h
+++ include/hw3.h
@@ -2,8 +2,17 @@
 
 typedef struct GameState
 {
-    // define your struct here
+    int rows;
+    int cols;
+    char** board;
+    int** stack_tiles;
+
+    //stack for memory
+    void *items[100]; // check it later
+    int top; 
+    
 } GameState;
+
 
 GameState* initialize_game_state(const char *filename);
 GameState* place_tiles(GameState *game, int row, int col, char direction, const char *tiles, int *num_tiles_placed);
