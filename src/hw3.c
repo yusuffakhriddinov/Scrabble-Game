f1c18e4cbb34e4550ac8d8bad6f3d781f9180a13
Yusuf Fakhriddinov <135459719+yusuffakhriddinov@users.noreply.github.com>
2024-03-23 19:13:41 +0000

    Your commit message here

--- src/hw3.c
+++ src/hw3.c
@@ -14,6 +14,9 @@ GameState* initialize_game_state(const char *filename) {
 
     GameState *result = (GameState *)malloc(sizeof(GameState));
 
+    result->top = 0;
+    result->items[result->top] = result;
+
     char ch;
     int rows;
     int cols;
@@ -27,7 +30,7 @@ GameState* initialize_game_state(const char *filename) {
     }
     
     rewind(file);
-
+    
     result->rows = rows;
     result->cols = cols; 
 
@@ -51,7 +54,6 @@ GameState* initialize_game_state(const char *filename) {
         fgetc(file);
     }
     
-    printf("%p\n", (void *)result);
     
 
     fclose(file);
@@ -66,22 +68,204 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
     (void)tiles;
     (void)num_tiles_placed;
 
-    // int placed = 0;
-    GameState *temp_struct = game+1;
+    
+    GameState *temp_struct = (GameState *)malloc(sizeof(GameState)); //temprorary
+
 
     int length = strlen(tiles);
+    // printf("%d\n", length);
 
-    //invalid parameters
+    // //legal check
     if(row<0 || row>=game->rows){
         return game;
     }else if(col<0 || col>=game->cols){
         return game;
     }else if(direction!='H' && direction!='V'){
         return game;
-    }else if(row+length>game->rows || col+length>game->cols){
-        return game;
     }
 
+    // // - check existence of tile in word.txt
+
+    // int tile_exist = 0;
+
+    // char word[100];
+    // char input_word[8];
+    // int empty_space = 0;
+
+    // if (direction=='H' || direction=='h'){
+    //     for(int i = 0; i<length; i++){
+    //         if (tiles[i]==' '){
+    //             input_word[i] = game->board[row][col+i];
+    //             empty_space=1;
+    //         }else{
+    //             input_word[i] = tiles[i];
+    //         }
+    //     }
+    // }
+
+    // if (direction=='V' || direction=='v'){
+    //     for(int i = 0; i<length; i++){
+    //         if (tiles[i]==' '){
+    //             input_word[i] = game->board[row+i][col];
+    //             empty_space=1;
+    //         }else{
+    //             input_word[i] = tiles[i];
+    //         }
+    //     }
+        
+    // }
+    
+    
+
+    // FILE *words_file = fopen("./tests/words.txt", "r");
+
+    // while (fscanf(words_file, "%s", word) != EOF) {
+    //     // Process each word
+    //     if (empty_space==1){
+    //         if(strcasecmp(input_word, word) == 0){
+    //             tile_exist = 1;
+    //         }
+    //     }else{
+    //         if(strcasecmp(tiles, word) == 0){
+    //             tile_exist = 1;
+    //         }
+    //     }
+        
+    // }
+
+
+    // if(tile_exist==0){
+    //     printf("here\n");
+    //     return game;
+    // }
+
+    // fclose(words_file);
+
+
+    // // - check if it is first word
+    // int first_word = 1;
+
+    // for(int i = 0; i<game->rows; i++){
+    //     for (int j = 0; j<game->cols; j++){
+    //         if (game->stack_tiles[i][j]>0){
+    //             first_word = 0;
+    //         }
+    //     }
+    // }
+
+    // if (first_word==1){
+    //     if (length<2){
+    //         return game;
+    //     }
+    // }else{
+
+    //     if (direction=='H' || direction=='h'){
+    //     int valid1 = 0;
+    //     int valid2 = 0;
+    //     for (int i = 0; i<length && col+i<game->cols; i++){
+    //         if (game->stack_tiles[row][col+i]!=0){
+    //             valid1=1;
+    //         }
+    //         if(game->board[row][col+i]!=tiles[i]){
+    //             valid2=1;
+    //         }
+    //     }
+
+
+    //     if(valid1==0){
+    //         return game;
+    //     }else if(valid2==0){
+    //         return game;
+    //     }
+    // }
+
+    // if (direction=='V' || direction=='v'){
+    //     int valid1 = 0;
+    //     int valid2 = 0;
+    //     for (int i = 0; i<length && row+i<game->rows; i++){
+    //         if (game->stack_tiles[row+i][col]!=0){
+    //             valid1=1;
+    //         }
+    //         if(game->board[row+i][col]!=tiles[i]){
+    //             valid2=1;
+    //         }
+    //     }
+
+    //     if(valid1==0){
+    //         return game;
+    //     }else if(valid2==0){
+    //         return game;
+    //     }
+    // }
+
+    // }
+
+
+    
+
+    
+    //board
+    if (direction=='H' || direction=='h'){
+        temp_struct->rows = game->rows;
+        
+        if(col+length>game->cols){
+            temp_struct->cols = col+length;
+        }else{
+            temp_struct->cols = game->cols;
+        }
+
+    }
+
+    if (direction=='V' || direction=='v'){
+        if (row+length>game->rows){
+            temp_struct->rows = row+length;
+        }else{
+            temp_struct->rows = game->rows;
+        }
+
+        temp_struct->cols = game->cols;
+
+    
+    }
+
+    temp_struct->board = (char **)malloc(temp_struct->rows * sizeof(char *));
+    for (int i = 0; i<temp_struct->rows; i++){
+        temp_struct->board[i] = (char *)malloc(temp_struct->cols * sizeof(char));
+        for (int j = 0; j<temp_struct->cols; j++){
+            temp_struct->board[i][j] = '.';
+        }
+    }
+
+
+    for(int i = 0; i<game->rows; i++){
+        for (int j = 0; j<game->cols; j++){
+            temp_struct->board[i][j] = game->board[i][j];
+                
+        }
+    }
+
+    // stack_tiles
+    temp_struct->stack_tiles = (int **)malloc(temp_struct->rows * sizeof(int *));
+
+    for (int i = 0; i<temp_struct->rows; i++){
+        temp_struct->stack_tiles[i] = (int *)malloc(temp_struct->cols * sizeof(int));
+        for (int j = 0; j<temp_struct->cols; j++){
+            temp_struct->stack_tiles[i][j] = 0;
+        }
+    }
+
+    for(int i = 0; i<game->rows; i++){
+        for (int j = 0; j<game->cols; j++){
+            temp_struct->stack_tiles[i][j] = game->stack_tiles[i][j];
+            
+        }   
+    }
+
+    //stack allocation
+    temp_struct->top = game->top;
+    temp_struct->items[temp_struct->top] = game;
+
+    int placed = 0;
 
     //Horizontal
     if (direction=='H' || direction=='h'){ 
@@ -90,9 +274,9 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
             if(tiles[i]==' '){
                 continue;
             }else{
-                game->board[row][col+i] = tiles[i];
-                game->stack_tiles[row][col+i]++;
-                // placed++;
+                temp_struct->board[row][col+i] = tiles[i];
+                temp_struct->stack_tiles[row][col+i]++;
+                placed++;
             }
             
         }
@@ -100,65 +284,61 @@ GameState* place_tiles(GameState *game, int row, int col, char direction, const 
 
     //Vertical
     if (direction=='V' || direction=='v'){ 
-        
+
         for(int i = 0; i<length; i++){
             if(tiles[i]==' '){
                 continue;
             }else{
-                game->board[row+i][col] = tiles[i];
-                game->stack_tiles[row+i][col]++;
-                // placed++;
+                temp_struct->board[row+i][col] = tiles[i];
+                temp_struct->stack_tiles[row+i][col]++;
+                placed++;
             }
             
         }
     }
-
     
-    temp_struct->rows = game->rows;
-    temp_struct->cols = game->cols;
+    *num_tiles_placed = placed;
+    printf("%d\n", *num_tiles_placed);
 
-    //board
-    temp_struct->board = (char **)malloc(temp_struct->rows * sizeof(char *));
-
-    for(int i = 0; i<temp_struct->rows; i++){
-        temp_struct->board[i] = (char *)malloc(temp_struct->cols * sizeof(char));
-        for (int j = 0; j<temp_struct->cols; j++){
-            temp_struct->board[i][j] = game->board[i][j];
-            
-        }
-       
-    }
-
-    //stack_tiles
-    temp_struct->stack_tiles = (int **)malloc(temp_struct->rows * sizeof(int *));
-
-    for(int i = 0; i<temp_struct->rows; i++){
-        temp_struct->stack_tiles[i] = (int *)malloc(temp_struct->cols * sizeof(int));
-        for (int j = 0; j<temp_struct->cols; j++){
-            temp_struct->stack_tiles[i][j] = game->stack_tiles[i][j];
-            
-        }
-       
-    }
-
-    
-
-    // num_tiles_placed = placed;
     
     game = temp_struct;
-    printf("%p\n", (void *)game);
+    
+    
+
+    game->top++;
+    game->items[game->top] = game;
+    
+
+    
+
     return game;
 }
 
 GameState* undo_place_tiles(GameState *game) {
-    (void)game;
-    game--;
-    // printf("%p\n", (void *)game);
-    return game;
+
+    if (game->top==0){
+        return game->items[0];
+    }
+    
+    return game->items[--game->top];
 }
 
 void free_game_state(GameState *game) {
-    (void)game;
+    while(game->top != 0) {
+        game = undo_place_tiles(game);
+    }
+    
+    for (int i = 0; i < game->rows; i++) {
+        free(game->board[i]);
+    }
+    free(game->board);
+        
+    for (int i = 0; i < game->rows; i++) {
+        free(game->stack_tiles[i]);
+    }
+    free(game->stack_tiles);
+
+    free(game);
 }
 
 void save_game_state(GameState *game, const char *filename) {
